7679b58b71cfb4d0d84335f7dc233f2427dc8f1b
andreajalba1 <andrea.jalba00@gmail.com>
2022-07-22 01:01:14 +0300

    Am adaugat exercitiul 1 pentru lesson6

---
+++ Exercitiul1.java
@@ -0,0 +1,11 @@
+public class Exercitiul1 {
+    public static void main(String[] args) {
+        int var1 = 12;
+        if ((var1 % 2 == 0) && (var1 > 10)) {
+            System.out.println("Numarul este par");
+        } else if ((var1 % 2 != 0) && (var1 < 15)) {
+            System.out.println("Numarul este impar");
+        }
+    }
+}
+
